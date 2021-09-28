abc262396e2594bba7ab39e6ed32884c5f1d5d1f
Alex <zthh626@gmail.com>
2021-09-27 20:54:41 -0700

    Add config and driver vars to main

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
 """ Main loop that is run on the Raspberry Pi on the USV 🛳⚓️ """
 
 from typing import List, Optional
+from multiprocessing import Process
 from serial import Serial
 from src.data_classes.sensor.data_in import GpsCoord
 
 serial: Optional[Serial] = None
+
 route: List[GpsCoord] = []
+paused_gps_coord: Optional[GpsCoord] = None
+driver_process: Optional[Process] = None
+
+with open(file="config.json", mode="r", encoding="utf-8") as file:
+    pass
 
 
 def run():
