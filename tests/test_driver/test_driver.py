bacddd3c3fb53e04389ec3cc53ac8bf8730cc721
John <john.mic.mckinnon@gmail.com>
2021-10-17 20:35:02 -0700

    add_gps_coord tests complete - for some reason git is showing other files have been modified I did not knowningly touch..

--- tests/test_driver/test_driver.py
+++ tests/test_driver/test_driver.py
@@ -5,30 +5,49 @@ from src.driver.driver import add_gps_coord
 
 class TestDriver(unittest.TestCase):
     
+
+    
     ###########################
     ## add_gps_coord() Tests ##
     ###########################
 
     def test_addGPSCoordNotNone(self):
-        """Tests add_grps_route will not return none"""
+        """Ensure function does not return none"""
         coord1 = GpsCoord(1, 2.00, 3.00)
         coord2 = GpsCoord(2, 4.00, 6.00)
         route = [coord1, coord2]
         self.assertIsNotNone( add_gps_coord(route, coord1) )
 
 
-    # Test you can add a coord to an empty route
     def test_addGPSCoordEmptyRoute(self):
-        """Succesfully add a coordinate to an empty route"""
-        pass
+        """Ensure adding a coordinate to an empty route is successful"""
+        emptyRoute = []
+        coord = GpsCoord(1, 2.00, 3.00)
+        result = [coord]
+        self.assertEqual(result, add_gps_coord(emptyRoute, coord))
 
 
-    # Check a new new coord was successsfully added
     def test_addGPSCoordNormalRoute(self):
-       """Successfully add a coordinate to a non-empty route"""
-       pass
+        """Ensure adding a coordinate to a non-empty route is successful"""
+        coord1 = GpsCoord(1, 2.00, 3.00)
+        coord2 = GpsCoord(2, 4.00, 6.00)
+        coord3 = GpsCoord(3, 6.00, 8.00)
+        route = [coord1, coord2]
+        result = [coord1, coord2, coord3]
+        self.assertEqual(result, add_gps_coord(route, coord3))
 
-    # 
+
+    def test_addGPSCoordDoesNotChangeList(self):
+        """Ensure the original route is not altered by adding a coordinate"""
+        coord1 = GpsCoord(1, 2.00, 3.00)
+        coord2 = GpsCoord(2, 4.00, 6.00)
+        coord3 = GpsCoord(3, 6.00, 8.00)
+        route = [coord1, coord2]
+        result = [coord1, coord2]
+        add_gps_coord(route, coord3)
+        self.assertEqual(route, result)
+
+
 
     ##############################
     ## remove_gps_coord() Tests ##
