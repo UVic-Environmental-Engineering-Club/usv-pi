50b30d404f7cf58e0e8d53d11e4e9496ef4f29c1
agrymaloski <48161305+agrymaloski@users.noreply.github.com>
2022-01-16 20:25:12 -0800

    Basic logic to read from serial (#21)
    * Basic logic to read from serial
    
    * removed serial function param
    
    * Update app.py
    
    removed redundant import
    
    * removed serial as param for serial process

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 """ Main loop that is run on the Raspberry Pi on the USV 🛳⚓️ """
-
 from typing import List, Optional, Dict, Callable, Any, Tuple
 from multiprocessing import Process, Manager
 from serial import Serial
@@ -11,15 +10,12 @@ from src.database.database_listener import setup_database_handlers
 from src.serial.serial_listener import setup_serial_handlers
 from src.socketio.socketio_listener import setup_socketio_handlers
 from src.data_classes.sensor.data_in import GpsCoord
+import json
 
-serial: Optional[Serial] = None
 
 route: List[GpsCoord] = []
 paused_gps_coord: Optional[GpsCoord] = None
 processes: Dict[str, Process] = {}
-
-with open(file="config.json", mode="r", encoding="utf-8") as file:
-    pass
 
 
 def run():
@@ -27,16 +23,12 @@ def run():
     with Manager() as manager:
         subscribers: Dict[EventType, List[Callable[[Any], Any]]] = manager.dict()
         event_list: List[Tuple[EventType, Any]] = manager.list()
-
         setup_database_handlers(manager, subscribers)
         setup_serial_handlers(manager, subscribers)
         setup_socketio_handlers(manager, subscribers)
-
         processes["driver"] = Process(target=driver_loop, args=(event_list,))
-        processes["serial"] = Process(target=serial_loop, args=(event_list,))
-        processes["event_loop"] = Process(
-            target=run_event_loop, args=(subscribers, event_list)
-        )
+        processes["serial"] = Process(target=serial_loop,  args=(event_list))
+        processes["event_loop"] = Process(target=run_event_loop, args=(subscribers, event_list))
 
         for _, process in processes.items():
             process.start()
--- src/serial/serial.py
+++ src/serial/serial.py
@@ -1,13 +1,27 @@
 """ Functions for things related to serial and pySerial """
-
+from os import system
 import time
+from serial import Serial
 from typing import List, Tuple, Any
 from src.events.event_type import EventType
-
+import json
 
 def serial_loop(event_list: List[Tuple[EventType, Any]]):
     """Serial loop with two threads, one for reading, one for writing"""
     """https://stackoverflow.com/questions/39127158/small-example-for-pyserial-using-threading"""
+
+    #read serial info and open
+    with open(file="config.json", mode="r", encoding="utf-8") as file:
+        config = json.load(file)
+        serial = Serial(config["port"], config["baudrate"], timeout = config["timeout"])
+        file.close()
+
     while True:
         print("serial process!")
-        time.sleep(2)
+        try : 
+            if(serial.is_open == True) : print(serial.read())
+        except: 
+            print ("Error reading from serial")
+            break
+        time.sleep(3) 
+
