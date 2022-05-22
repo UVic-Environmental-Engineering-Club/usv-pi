7b834399cb651340eff4b606d973e187e2c56c3d
Philip <philipesclamado@uvic.ca>
2022-05-21 20:42:52 -0700

    feat: route-shore list management

--- src/socketio/namespace.py
+++ src/socketio/namespace.py
@@ -1,4 +1,4 @@
-from src.constants import ROUTE
+from src.constants import ROUTE, SHORE
 import socketio
 
 
@@ -13,13 +13,28 @@ class USVNameSpace(socketio.AsyncClientNamespace):
 
     async def on_init_route(self, data):
         print("init route", data)
-        ROUTE = data
+        ROUTE.append(data)
 
-    async def on_add_point_ack(self, data):
-        print(data)
+    async def on_add_point_route_ack(self, data):
+        print("add point route", data)
+        ROUTE.append(data)
+    
+    async def on_add_point_shore_ack(self, data):
+        print("add point shore", data)
+        SHORE.append(data)
 
-    async def on_delete_point_ack(self, data):
-        print(data)
+    async def on_delete_point_route_ack(self, data):
+        print("delete point route", data)
+        ROUTE.append(data)
+
+    async def on_delete_point_shore_ack(self, data):
+        print("delete point shore", data)
+        SHORE.append(data)
 
     async def on_clear_route_ack(self, data):
-        print(data)
+        print("clear route", data)
+        ROUTE.clear()
+
+    async def on_clear_shore_ack(self, data):
+        print("clear shore", data)
+        SHORE.clear()
