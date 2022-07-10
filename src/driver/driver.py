95eb31c3b3479d4dc73930e747481f52a8969938
Philip <philipesclamado@uvic.ca>
2022-07-10 15:28:47 -0700

    feat: boundary calculation

--- src/driver/driver.py
+++ src/driver/driver.py
@@ -1,16 +1,22 @@
 """ Functions that drive the boat """
 
+import math
 import asyncio
 import arrow
-from typing import List
-from src.constants import GPS_DATA_COLLECTION
-from src.data_classes.sensor.data_in import GpsCoord
 
+from src.data_classes.sensor.data_in import GpsCoord
+from src.constants import DATA, GPS_DATA_COLLECTION
        
 
 async def driver_loop():
     #adjust rudder loop
     nextIteration = arrow.now().replace(second = 0, microsecond = 0).shift(seconds=10)
+    """Driver process logic lives in here"""
+
+    current_point = DATA["route"].pop(0)
+    db_usv_point = GPS_DATA_COLLECTION.find_one()
+    usv_point = GpsCoord(arrow.now(), db_usv_point["long"], db_usv_point["lat"])
+    print(boundary_calc(usv_point, current_point))
     while True:
         if arrow.now() < nextIteration:
             await asyncio.sleep(0.001)
@@ -22,31 +28,18 @@ async def driver_loop():
         currentPoint = GPS_DATA_COLLECTION.find().limit(1).sort({'$natural':-1})
 
 
+def boundary_calc(point_a: GpsCoord, point_b: GpsCoord) -> tuple[GpsCoord, GpsCoord]:
+    c = GpsCoord(arrow.now(), 0, 0)
+    d = GpsCoord(arrow.now(), 0, 0)
 
-def add_gps_coord(route: List[GpsCoord], coord: GpsCoord) -> List[GpsCoord]:
-    """Adds a GpsCoord to the route"""
-    return route + [coord]
+    slope = (point_b.long - point_a.long)/(point_b.lat - point_a.lat)
 
+    dy = math.sqrt(3**2/(slope**2+1))
+    dx = -slope*dy
 
-def remove_gps_coord(route: List[GpsCoord], coord: GpsCoord) -> List[GpsCoord]:
-    """Removes a GpsCoord to the route"""
-    return [curCoord for curCoord in route if curCoord != coord]
+    c.lat = point_b.lat + dx
+    c.long = point_b.long + dy
+    d.lat = point_b.lat - dx
+    d.long = point_b.long - dy   
 
-
-def reset_route() -> List[GpsCoord]:
-    """Returns an empty list"""
-    return []
-
-
-def start_route(route: List[GpsCoord], start_target: GpsCoord) -> None:
-    """
-    Starts a process (multiprocess from another core) to follow a route.
-    Process will start from current location from start_target,
-    start_target is in case it was paused previously
-    """
-    pass
-
-
-def pause_route(route: List[GpsCoord]) -> GpsCoord:
-    """Pauses the route and returns the node that it was currently going to"""
-    pass
+    return (c, d)
