50920e316a007b28d56f94fb685d47976c52a628
Philip Esclamado <philipesclamado@uvic.ca>
2022-09-17 16:50:31 -0700

    feat: driving algorithm (#34)
    * feat: route-shore list management
    
    * feat: route-shore list management
    
    * chore: deleted unused module
    
    * Initialize while loop for adjusting rudders.
    
    * refactor: use gps_data instead of sensor_data
    
    * getting current gps point fron database
    
    * fix: remove database name
    
    * changing driver loop
    
    * SOFTWARE-1: parse_string method (#32)
    
    * SOFTWARE-1: parse_string method
    
    * feat: add git command to readme
    
    * feat: update parse_input
    
    Co-authored-by: seyedmehradadimi <seyedmehradadimi@uvic.ca>
    
    * fix: natural
    
    * feat: boundary calculation
    
    * debug: sort
    
    * added docstring
    
    * feat: read and update database from sensors
    
    * fix(driver-loop): merge conflicts
    
    * feat: update baud rate
    
    * feat: boundary detection
    
    * fix(driver.py): deleted duplicates from last push
    
    * feat: collision detection
    
    Co-authored-by: Sofia <sofia.scholefield@gmail.com>
    Co-authored-by: Alex <zthh626@gmail.com>
    Co-authored-by: Alex Huang <31324580+zthh626@users.noreply.github.com>
    Co-authored-by: seyedmehradadimi <seyedmehradadimi@uvic.ca>

---
+++ src/collision_detection/collision_detection.py
@@ -0,0 +1,93 @@
+import asyncio
+from enum import Enum, auto
+from typing import Tuple, Union
+
+from src.constants import DATA, LID_COLLECTION, State
+from src.driver import driver
+
+
+class ShoreDirection(Enum):
+    LEFT = auto()
+    RIGHT = auto()
+
+
+class CDState(Enum):
+    TURN_LEFT = auto()
+    TURN_RIGHT = auto()
+    CLEAR_OBSTACLE_LEFT = auto()
+    CLEAR_OBSTACLE_RIGHT = auto()
+
+
+def get_lid_data() -> Tuple[int, int, int]:
+    lid_doc = LID_COLLECTION.find_one(sort=[("timestamp", -1)])
+    lid_data = lid_doc["data"]
+
+    left = int(lid_data["left"]) / 10
+    middle = int(lid_data["middle"]) / 10
+    right = int(lid_data["right"]) / 10
+
+    return (left, middle, right)
+
+
+async def collision_detection_loop():
+    """Lidar data is formatted is XYZ,
+    where X is 10s, Y is ones, and Z is a decimal
+    Units: meters
+    """
+    cd_state: Union[None, CDState] = None
+    prev_state: Union[None, State] = None
+
+    while True:
+        await asyncio.sleep(1)
+
+        left, middle, right = get_lid_data()
+
+        if middle > 20 and cd_state is None:
+            continue
+
+        if DATA["state"] != State.COLLISION_DETECTION:
+            prev_state = DATA["state"]
+
+        DATA["state"] = State.COLLISION_DETECTION
+
+        if cd_state is None:
+            cd_state = (
+                CDState.TURN_LEFT
+                if locate_shore() == ShoreDirection.RIGHT
+                else CDState.TURN_RIGHT
+            )
+
+        print("after", cd_state)
+
+        if cd_state is CDState.TURN_LEFT:
+            while middle < 20 or right < 20:
+                driver.turn_left()
+                await asyncio.sleep(1)
+                left, middle, right = get_lid_data()
+
+            cd_state = CDState.CLEAR_OBSTACLE_LEFT
+
+        if cd_state is CDState.TURN_RIGHT:
+            while middle < 20 or left < 20:
+                driver.turn_right()
+                await asyncio.sleep(1)
+                left, middle, right = get_lid_data()
+
+            cd_state = CDState.CLEAR_OBSTACLE_RIGHT
+
+        if cd_state in (
+            CDState.CLEAR_OBSTACLE_LEFT,
+            CDState.CLEAR_OBSTACLE_RIGHT,
+        ):
+            if (cd_state == CDState.CLEAR_OBSTACLE_LEFT and left < 20) or (
+                cd_state == CDState.CLEAR_OBSTACLE_RIGHT and right < 20
+            ):
+                driver.forward()
+            else:
+                cd_state = None
+                DATA["state"] = prev_state
+                prev_state = None
+
+
+def locate_shore() -> ShoreDirection:
+    return ShoreDirection.LEFT
--- src/serial/parse_input.py
+++ src/serial/parse_input.py
@@ -1,6 +1,43 @@
-def parse_string(message: str) -> str:
+from typing import Dict
 
-    split_message = message.split("-")
-    type = split_message.pop(0)
+from src.constants import SENSOR_NAMES
 
-    return {"type": type, "data": split_message}
+
+def parse_string(message: str) -> Dict[str, float]:
+    if message is not None:
+        split_message = message.split(":")
+        message_type = split_message[0]
+        parsed_message = {"type": message_type, "data": {}}
+
+        if message_type not in SENSOR_NAMES:
+            return {}
+
+        if message_type in ("ACC", "GYR", "MAG"):
+            parsed_message["data"] = {
+                "x": split_message[1],
+                "y": split_message[2],
+                "z": split_message[3],
+            }
+        elif message_type in ("LID"):
+            parsed_message["data"] = {
+                "left": split_message[1],
+                "middle": split_message[2],
+                "right": split_message[3],
+            }
+        elif message_type in ("BAT"):
+            parsed_message["data"] = {
+                "volatage": split_message[1],
+            }
+        elif message_type in ("RPM", "TMP", "WET"):
+            parsed_message["data"] = {
+                "left": split_message[1],
+                "right": split_message[2],
+            }
+        elif message_type in ("GPS"):
+            parsed_message["data"] = {"long": split_message[1], "lat": split_message[2]}
+        elif message_type in ("GPSSTAT"):
+            parsed_message["data"] = {"sats": split_message[1], "fix": split_message[2]}
+
+        return parsed_message
+
+    return {}
