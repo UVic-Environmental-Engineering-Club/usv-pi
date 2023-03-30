5834c6ceeb9647acf774894147bc513f6a60aece
Philip <philipesclamado@uvic.ca>
2023-03-30 15:24:35 -0700

    feat: helper fxn for navigation

--- src/driver/driver.py
+++ src/driver/driver.py
@@ -94,3 +94,55 @@ def bound_detect(
 def geographic_to_UTM(point: GpsCoord) -> tuple[float, float, float]:
     """Converts a point from geographic coordinates to UTM coordinates"""
     return utm.from_latlon(point.lat, point.long)
+
+
+def heading(x, y) -> float:
+    """This formula gives the direction of the (x, y) vector counted clockwise from the y axis.
+    The correct formula depends then on how the magnetometer has been mounted relative to the vehicle.
+    If we assume that the x axis of the magnetometer points forward and the y axis points to the left, then:
+
+    - the vehicle is heading north (0° magnetic heading) when the horizontal component of the magnetic field is along +x
+    - the vehicle is heading east (90° magnetic heading) when the horizontal component of the magnetic field is along +y
+
+    NOTE: Magnetic declination has to be added if one is interested in the true (rather than magnetic) heading.
+    -> https://arduino.stackexchange.com/questions/18625/converting-three-axis-magnetometer-to-degrees/88707#88707"""
+    return math.atan2(y, x) * 180 / math.pi
+
+
+def geographic_to_cartesian(point: GpsCoord) -> tuple[float, float, float]:
+    """Converts a point from geographic coordinates to cartesian coordinates"""
+    x = math.cos(point.lat) * math.cos(point.long)
+    y = math.cos(point.lat) * math.sin(point.long)
+    z = math.sin(point.lat)
+
+    return (x, y, z)
+
+
+def get_bearing(point_a: GpsCoord, point_b: GpsCoord) -> float:
+    """Calculate the bearing between two points"""
+    x = math.sin(point_b.long - point_a.long) * math.cos(point_b.lat)
+    y = math.cos(point_a.lat) * math.sin(point_b.lat) - math.sin(
+        point_a.lat
+    ) * math.cos(point_b.lat) * math.cos(point_b.long - point_a.long)
+    return math.atan2(x, y)
+
+
+def get_heading(point_a: GpsCoord, point_b: GpsCoord) -> float:
+    """Calculate the heading between two points"""
+    return math.atan2(point_b.long - point_a.long, point_b.lat - point_a.lat)
+
+
+def get_rudder_angle(point_a: GpsCoord, point_b: GpsCoord) -> float:
+    """Calculate the rudder angle between two points"""
+    return (
+        math.atan2(point_b.long - point_a.long, point_b.lat - point_a.lat)
+        * 180
+        / math.pi
+    )
+
+
+def get_distance(point_a: GpsCoord, point_b: GpsCoord) -> float:
+    """Calculate the distance between two points"""
+    return math.sqrt(
+        (point_a.lat - point_b.lat) ** 2 + (point_a.long - point_b.long) ** 2
+    )
