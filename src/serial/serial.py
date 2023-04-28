0a61bbafabb4bd3e0aa3817f5363c243c98babb2
daehwankim112 <kdw8731@gmail.com>
2023-04-28 04:29:28 -0700

    chore: basic threading for serial reading and writing using 2 threads

--- src/serial/serial.py
+++ src/serial/serial.py
@@ -1,14 +1,51 @@
 """ Functions for things related to serial and pySerial """
 import asyncio
+import threading """Don't forget to add it on piplock"""
 from src.events.event_type import EventType
 from src.events.events import post_event
 from src.constants import SERIAL
+
+thread_flag = None
+
+
+def reading():
+    global thread_flag
+    thread_flag = 'reading'
+    while thread_flag != 'reading': asyncio.sleep( 0.001 )
+
+    while thread_flag == 'reading':
+        """Do reading"""
+        if SERIAL.is_open == True:
+            message = SERIAL.readline().decode("utf8").strip()
+            if message:
+                await post_event(EventType.SERIAL_IN, message)
+
+
+    thread_flag = "reading done"
+
+
+def writing(): """Figure out what do we write"""
+    global thread_flag
+    thread_flag = 'writing'
+    while thread_flag != 'writing': asyncio.sleep( 0.001 )
+
+    while thread_flag = 'writing':
+        """Do writing"""
+        if SERIAL.is_open == True:
+            message = """What is the message?"""
+            if message:
+                await post_event(EventType.SERIAL_OUT, message)
+    
+
+    thread_flag = "writing done"
+
+
 
 
 async def serial_loop():
     """Serial loop with two threads, one for reading, one for writing"""
     """https://stackoverflow.com/questions/39127158/small-example-for-pyserial-using-threading"""
-    await asyncio.sleep(1)
+    await asyncio.sleep(1) 
 
     while True:
         await asyncio.sleep(0.001)
@@ -17,10 +54,13 @@ async def serial_loop():
             continue
 
         try:
-            if SERIAL.is_open == True:
-                message = SERIAL.readline().decode("utf8").strip()
-                if message:
-                    await post_event(EventType.SERIAL_IN, message)
+            t1 = threading.Thread(target = reading)
+            t2 = threading.Thread(target = writing, args=[]) """Figure out what is args for this case"""
+            t1.start()
+            asyncio.sleep(0.5) """This could be the reading/writing speed"""
+            t2.start()
+            asyncio.sleep(0.5) """This could be the reading/writing speed"""
+
 
         except Exception as e:
             print("Error reading from serial", e)
